dc53b23be409126d594ee81ce0a7a67b43b06fdb
Arefcy Saban <andriyanto.ay595@gmail.com>
2025-12-20 21:02:31 +0800

    test

--- tambah.php
+++ tambah.php
@@ -9,5 +9,33 @@ if (isset($_POST['simpan'])) {
     mysqli_query($conn,
         "INSERT INTO buku (judul, penulis, tahun)
          VALUES ('$judul', '$penulis', '$tahun')");
+
+    // pindah ke halaman utama setelah simpan
+    header("Location: index.php");
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tambah Buku</title>
+</head>
+<body>
+
+<h2>Tambah Data Buku</h2>
+
+<form method="post">
+    <label>Judul</label><br>
+    <input type="text" name="judul" required><br><br>
+
+    <label>Penulis</label><br>
+    <input type="text" name="penulis" required><br><br>
+
+    <label>Tahun</label><br>
+    <input type="number" name="tahun" required><br><br>
+
+    <button type="submit" name="simpan">Simpan</button>
+</form>
+
+</body>
+</html>
